27228c76f344bd37ecc82e8f35a49d09d52c2bb6
KH Moon <67881688+drmoon-1st@users.noreply.github.com>
2022-08-13 15:43:49 +0900

    Add files via upload

---
+++ javascript/exercise4.js
@@ -0,0 +1,30 @@
+// for (var i=0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// var a = [1, 2, 3, 4, 'hi'];
+// a.key = 'value';
+
+// a.forEach(consolelog);
+
+// function consolelog(element, idx, arr) {
+//     console.log(arr);
+// }
+
+// var string = '문자열';
+// console.log(string);
+
+// var a = '123';
+// console.log(typeof a);
+
+// a = 123;
+// console.log(typeof a);
+
+// var a = 3;
+// let b = (5 < a) ? 50 : 40;
+// console.log(b);
+
+console.log(1 && 2);
+console.log(1 || 2);
+console.log(1 && 0);
+console.log(1 || 0);
