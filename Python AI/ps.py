68334fa59cc8153a49a8e43532475bf19ea792f2
KH Moon <67881688+drmoon-1st@users.noreply.github.com>
2022-10-14 21:48:17 +0900

    Add files via upload

--- Python AI/ps.py
+++ Python AI/ps.py
@@ -1,26 +1,20 @@
 import sys
+from itertools import combinations, permutations
 
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-data = [] # ti, pi 저장
-arr = [0 for _ in range(n+1)] # 최고값 저장
+comb = list(combinations([x for x in range(n)], n//2))
+front, end = 0, len(comb)-1
 
-for i in range(n):
-    t, p = map(int, sys.stdin.readline().split())
-    data.append([t, p])
-
-# for i in range(n-1, -1, -1):
-#     if data[i][0] + i > n:
-#         arr[i] = arr[i+1]
-#     else:
-#         arr[i] = max(arr[i+1], arr[i + data[i][0]] + data[i][1])
-
-# print(arr[0])
-
-for i in range(1, n+1):
-    if data[i-1][0] + i > n:
-        arr[i] = arr[i-1]
-    else:
-        arr[i] = max(arr[i-1], arr[i + data[i-1][0]] + data[i-1][1])
-
-print(arr)
+answer = inf
+while front < end:
+    start = link = 0
+    for a, b in list(permutations(comb[front], 2)):
+        start += graph[a][b]
+    for a, b in list(permutations(comb[end], 2)):
+        link += graph[a][b]
+    answer = min(answer, ((start-link)**2)**0.5)
+    front += 1;end -= 1
+print(int(answer))
