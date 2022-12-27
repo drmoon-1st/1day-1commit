e5e51fdb5779a11ab55cb2030034791592cd1f29
KH Moon <67881688+drmoon-1st@users.noreply.github.com>
2022-12-27 20:39:49 +0900

    Add files via upload

--- Python AI/ps.py
+++ Python AI/ps.py
@@ -1,20 +1,8 @@
 import sys
-from itertools import combinations, permutations
+from itertools import combinations_with_replacement
 
 input = sys.stdin.readline
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-comb = list(combinations([x for x in range(n)], n//2))
-front, end = 0, len(comb)-1
-
-answer = inf
-while front < end:
-    start = link = 0
-    for a, b in list(permutations(comb[front], 2)):
-        start += graph[a][b]
-    for a, b in list(permutations(comb[end], 2)):
-        link += graph[a][b]
-    answer = min(answer, ((start-link)**2)**0.5)
-    front += 1;end -= 1
-print(int(answer))
+n, m = map(int, input().split(' '))
+arr = sorted(list(map(int, input().split(' '))))
+for i in sorted(set(combinations_with_replacement(arr, m))):
+    print(*i)
