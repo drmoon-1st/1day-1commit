4e99327bf555c0ca651c2bcb107049ee30134a96
KH Moon <67881688+drmoon-1st@users.noreply.github.com>
2023-12-26 16:23:15 +0900

    Add files via upload

---
+++ C++/23_12_26.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+ // std::ios_base::sync_with_stdio(false);
+ // std::cin.tie(NULL);
+ // std::cout.tie(NULL);
+
+ long long a, b, c;
+ long long d = 0;
+
+ scanf("%lld %lld %lld", &a, &b, &c);
+
+ d = a + b + c;
+
+ printf("%lld \n", d);
+
+ return 0;
+}
